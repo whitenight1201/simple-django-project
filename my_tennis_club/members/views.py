74016f23eb4a7c7507ad00636775c95759bc5446
whitenight1201 <whitenight1201021@gmail.com>
2024-04-03 19:33:23 +0200

    add/cookie

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -33,15 +33,32 @@ def testing(request):
 
 
 def setcookie(request):
-    html = HttpResponse("<h1>Welcome to Cookie</h1>")
-    html.set_cookie("TechVidvan", "We are setting a cookie", max_age=None)
+    html = HttpResponse("<h1>Welcome to TechVidvan Employee Portal</h1>")
+    if request.COOKIES.get("visits"):
+        html.set_cookie("TechVidvan", "Welcome Back")
+        value = int(request.COOKIES.get("visits"))
+        html.set_cookie("visits", value + 1)
+    else:
+        value = 1
+        text = "Welcome for the first time"
+        html.set_cookie("visits", value)
+        html.set_cookie("TechVidvan", text)
     return html
 
 
 def showcookie(request):
-    show = request.COOKIES["TechVidvan"]
-    html = "<center> New Page <br>{0}</center>".format(show)
-    return HttpResponse(html)
+    if request.COOKIES.get("visits") is not None:
+        value = request.COOKIES.get("visits")
+        text = request.COOKIES.get("TechVidvan")
+        html = HttpResponse(
+            "<center><h1>{0}<br>You have requested this page {1} times</h1></center>".format(
+                text, value
+            )
+        )
+        html.set_cookie("visits", int(value) + 1)
+        return html
+    else:
+        return redirect("/setcookie")
 
 
 def updating_cookie(request):
